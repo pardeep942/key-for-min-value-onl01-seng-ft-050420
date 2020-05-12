1baad728c4ea3ce6f3d66470d6b5ef81c1cba2a2
PARDEEP KAUR <pkaur84@aol.com>
2020-05-12 21:04:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,3 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_a = name_hash.to_a
+
+  # Default key value
+  d_value= 1000
+  d_key= 0
+
+  # Iterate new array 
+  name_a.each do |i|
+      # If current value is lower than default, change value&key 
+      if i[1] < d_value
+        d_value = i[1]
+        d_key = i[0]
+      end
+
+   end
+
+  return d_key
+
+end  
