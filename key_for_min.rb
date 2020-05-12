9fe839d630b9c7dc2e2ec7a9dd10ebb7bea23350
PARDEEP KAUR <pkaur84@aol.com>
2020-05-12 21:05:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,7 +13,7 @@ def key_for_min_value(name_hash)
       # If current value is lower than default, change value&key 
       if i[1] < d_value
         d_value = i[1]
-        d_key = i[0]
+        d_key = i[1]
       end
 
    end
